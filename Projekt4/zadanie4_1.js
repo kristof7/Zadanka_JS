9782959b36c2ed80e938cb1cf7bc511f9deb7b41
Krzysztof Kampa <krzysztofkampa7@gmail.com>
2020-05-22 16:29:52 +0200

    projekt4/zadanie_1b (zmiana koloru tla przy ruchu kursora)

--- Projekt4/zadanie4_1.js
+++ Projekt4/zadanie4_1.js
@@ -3,4 +3,6 @@
 document.body.addEventListener("mousemove", (event) => {
     const text = document.querySelector("h1");
     text.innerText = `${event.clientX}, ${event.clientY}`;
+
+    document.body.style.backgroundColor = `rgb(${event.clientX /6.5}, ${event.clientY /3.5}, ${event.clientX / 6.5})`;
 });
