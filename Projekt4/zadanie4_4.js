faf5b1872eb2e05c2bf2d951ba7378ae9c481e86
Krzysztof Kampa <krzysztofkampa7@gmail.com>
2020-05-22 20:16:29 +0200

    projekt4/zadanie_4 (rozjasnianie/sciamnianie strony html) wersja 2

--- Projekt4/zadanie4_4.js
+++ Projekt4/zadanie4_4.js
@@ -5,18 +5,14 @@ let blue = 100;
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
 const changeColor = (e) => {
-    if (e.keyCode === 38 && red < 255) {
-            console.log("key up");
-            red++;
-            green++;
-            blue++;
-        } else if (e.keyCode === 40 && red > 0) {
-            console.log("key down");
-            red--;
-            green--;
-            blue--;
-        }
-            document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    switch (e.keyCode) {
+        case 38:
+            document.body.style.backgroundColor = `rgb(${red < 255 ? ++red : red}, ${green < 255 ? ++green : green }, ${blue < 255 ? ++blue : blue})`
+            break;
+        case 40:
+            document.body.style.backgroundColor = `rgb(${red > 0 ? --red : red}, ${green > 0 ? --green : green }, ${blue > 0 ? --blue : blue})`
+            break;
+    }
 };
 
 window.addEventListener("keydown", changeColor);
