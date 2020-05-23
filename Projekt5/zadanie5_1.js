416009d97f3b5b5bd1c94eb5deb97626f6a5c432
Krzysztof Kampa <krzysztofkampa7@gmail.com>
2020-05-23 15:59:01 +0200

    projekt5/zadanie_1 dodanie pliku js (wpisz haslo i uzyskaj wiadomosc)

---
+++ Projekt5/zadanie5_1.js
@@ -0,0 +1,23 @@
+const input = document.getElementById("pass");
+const div = document.querySelector(".message");
+
+const password = "user";
+const message = "Sprawdź mnie: https://github.com/kristof7";
+
+input.addEventListener("input", (e) => {
+// console.log(e.target.value);
+if(password === e.target.value) {
+    div.textContent = message;
+    e.target.value = "";
+} else {
+    div.textContent = "";
+}
+});
+
+input.addEventListener("focus", (e) => {
+    e.target.classList.add("active");
+});
+
+input.addEventListener("blur", (e) => {
+    e.target.classList.remove("active");
+});
