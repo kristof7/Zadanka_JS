19304c5b018b0416259725ff412f7a7d0eb21e6a
Krzysztof Kampa <krzysztofkampa7@gmail.com>
2020-05-24 17:21:40 +0200

    projekt5/zadanie_2 version2 - add ignoreCase function (write password and recive message)

--- Projekt5/zadanie5_2.js
+++ Projekt5/zadanie5_2.js
@@ -3,14 +3,18 @@ const div = document.querySelector("div");
 const passwords = ["Krzysztof", "wiosna", "user", "123", "111", "jedEN", "DwA"];
 const messages = ["Sprawdź mnie: https://github.com/kristof7", "piekna pora roku", "Witaj na mojej stronie", "..456789", "111 dalmatynczykow", "super", "działa!"];
 
+const LCpasswords = passwords.map(password => password.toLowerCase());
+
 const showMessage = (e) => {
-    passwords.forEach((elem, index) => {
-        if (e.target.value.toUpperCase() == elem.toUpperCase() ) {
-        div.innerText = messages[index];
-        e.target.value = ""; 
+    const textInput = e.target.value.toLowerCase();
+
+    for (let i = 0; i < LCpasswords.length; i++) {
+        if (textInput === LCpasswords[i]) {
+            div.innerText = messages[i];
+            e.target.value = "";
+        }
     }
-    });
-}
+};
 
 input.addEventListener("input", showMessage);
 
