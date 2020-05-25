94e6fd09f2d3e63b4b1d237bd0ba8d88e76f0daf
Krzysztof Kampa <krzysztofkampa7@gmail.com>
2020-05-25 14:30:47 +0200

    projekt6/zadanie_1 add prefix text to the drawn names

--- projekt6/zadanie6_1.js
+++ projekt6/zadanie6_1.js
@@ -2,12 +2,13 @@ const btn = document.querySelector("button");
 const div = document.querySelector("div");
 
 const names = ["Karolina", "Aleksandra", "Klaudia", "Sylwia", "Anastazja", "Angelika", "Ania"];
+const prefix = ["Wydaje mi się", "Mam wrażenie", "Szczerze powiedziawszy", "Szczerze uważam", "Jestem pewien", "Na 100% twierdzę"];
 
 
 const nameGenerator = () => {
     const index = Math.floor(Math.random() * names.length);
-    console.log(index);
-    div.innerText = `Najfajniejsze imię to ${names[index]}`
+    const indexPrefix = Math.floor(Math.random() * prefix.length);
+    div.innerText = `${prefix[indexPrefix]}, że najlepsze będzie imię ${names[index]}`
 };
 
 btn.addEventListener("click", nameGenerator)
