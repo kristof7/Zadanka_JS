a1b7dcf593e089f0364cce5102b226d7dd4b69a2
Krzysztof Kampa <krzysztofkampa7@gmail.com>
2020-06-13 14:54:41 +0200

    projekt8/zadanie_2 typing effect with display text

--- Projekt8/zadanie8_2.js
+++ Projekt8/zadanie8_2.js
@@ -1,13 +1,29 @@
 const spnText = document.querySelector('.text');
 const spnCursor = document.querySelector('.cursor');
-const txt = ['tekst1', 'tekst2', 'tekst3']
+const txt = ['DZIEŃ DOBRY', 'FAJNIE, ŻE JESTEŚ :)', 'OBEJRZYJ MOJE REPOZYTORIUM NA https://github.com/kristof7']
+
+let activeLetter = 0;
+let activeWord = 0;
 
 const addLetter = () => {
+
+    spnText.textContent += txt[activeWord][activeLetter];
+    activeLetter++;
+    if (activeLetter === txt[activeWord].length) {
+        activeWord++;
+        if(activeWord === txt.length) return;
+        return setTimeout(() => {
+            activeLetter = 0;
+            spnText.textContent = "";
+            addLetter();
+        }, 1000);
+    };
+setTimeout(addLetter, 100);
 }
 
 addLetter(); 
 
 const cursorAnimation = () => {
- spnCursor.classList.toggle('active');
+    spnCursor.classList.toggle('active');
 }
 setInterval(cursorAnimation, 400);
