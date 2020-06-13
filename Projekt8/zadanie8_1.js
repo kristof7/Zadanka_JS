78368b0809bd6ce6471afba1c1cc823f48ecda02
Krzysztof Kampa <krzysztofkampa7@gmail.com>
2020-06-13 10:06:20 +0200

    projekt8/zadanie_1 flashing cursor animation added

--- Projekt8/zadanie8_1.js
+++ Projekt8/zadanie8_1.js
@@ -1,8 +1,9 @@
 const spnText = document.querySelector(".text");
+const spnCursor = document.querySelector(".cursor");
 const txt = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore tenetur rerum ad? Eum quos reprehenderit minima! Fugit, omnis! Eveniet repellendus et iure deserunt iusto quas in est odit, labore optio? END"
 
 let indexText = 0;
-const time = 50;
+const time = 40;
 
 const addLetter = () => {
     spnText.textContent += txt[indexText];
@@ -10,4 +11,9 @@ const addLetter = () => {
     if (indexText === txt.length) clearInterval(indexTyping);
 }
 const indexTyping = setInterval(addLetter, time);
+
+const cursorAnimation = () => {
+    spnCursor.classList.toggle("active");
+}
+setInterval(cursorAnimation, 400);
      
